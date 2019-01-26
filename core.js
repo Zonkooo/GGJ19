0657f21a946f83ac8ba32fbba9bee0d1da2599e2
Raphaël Vandon <r.vandon@criteo.com>
2019-01-26 11:36:01 -0300

    can move thing around

---
+++ core.js
@@ -0,0 +1,133 @@
+var Vec2 = planck.Vec2;
+
+var preloadCount = 0;
+var preloadTotal = 0;
+
+var imgPlayer = new Image();
+
+var stage;
+var world;
+
+var box;
+var boxShape;
+
+var scale = 50;
+
+function startGame()
+{
+	stage = new createjs.Stage("gameCanvas");
+	var text = new createjs.Text("Loading...");
+	text.x = stage.canvas.width/2; text.y = stage.canvas.height/2;
+	text.textAlign = "center"; text.textBaseline = "middle";
+	stage.addChild(text);
+	stage.update();
+
+	preloadAssets();
+}
+
+function preloadAssets()
+{
+	launchGame();
+	//imgPlayer.onload = preloadUpdate;
+	//imgPlayer.src = "media/junglechar.png";
+
+	//createjs.Sound.addEventListener("fileload", preloadUpdate);
+	//createjs.Sound.registerSound("media/receive.wav", "jump", 4);
+}
+
+function preloadUpdate()
+{
+	preloadCount++;
+	if(preloadCount === preloadTotal)
+		launchGame();
+}
+
+function launchGame()
+{
+	stage.removeChildAt(0); //loading text
+
+	world = planck.World();
+
+	var walls = world.createBody({
+		type: 'static',
+		position: Vec2(.1, .1),
+	});
+
+	var wallFD = {
+		density: 0.0,
+		restitution: 0.1,
+	};
+	walls.createFixture(planck.Edge(Vec2(0, 0), Vec2(10, 0)), wallFD);
+	walls.createFixture(planck.Edge(Vec2(0, 0), Vec2(0, 10)), wallFD);
+	walls.createFixture(planck.Edge(Vec2(0, 10), Vec2(10, 10)), wallFD);
+	walls.createFixture(planck.Edge(Vec2(10, 0), Vec2(10, 10)), wallFD);
+
+	var boxFD = {
+		density: 1.0,
+		friction: 0.3,
+	};
+	box = world.createDynamicBody(Vec2(2, 2));
+	box.createFixture(planck.Box(1, 1), boxFD);
+
+	boxShape = new createjs.Shape();
+
+	var mouseGround = world.createBody();
+	var mouseJoint;
+	boxShape.on("mousedown", function(evt) {
+		var clickx = evt.stageX/scale;
+		var clicky = evt.stageY/scale;
+		mouseJoint = planck.MouseJoint({maxForce: 1000}, mouseGround, box, Vec2(clickx, clicky));
+		world.createJoint(mouseJoint);
+	});
+	boxShape.on("pressup", function(evt) {
+		world.destroyJoint(mouseJoint);
+		mouseJoint = null;
+	});
+	stage.on("stagemousemove", function(evt) {
+		var clickx = evt.stageX/scale;
+		var clicky = evt.stageY/scale;
+		point = { x: clickx, y: clicky };
+		if(mouseJoint) mouseJoint.setTarget(point);
+	});
+	stage.addChild(boxShape);
+
+	//var objBg = new createjs.Bitmap(imgBg);
+	//stage.addChild(objBg);
+
+	createjs.Ticker.framerate = 60;
+	createjs.Ticker.addEventListener("tick", update);
+}
+
+function update(event)
+{
+	world.step(1 / 60);
+
+	// var line = new createjs.Shape();
+	// line.graphics.setStrokeStyle(3);
+	// line.graphics.beginStroke("red");
+	// line.graphics.moveTo(10, 10);
+	// line.graphics.lineTo(10, 50);
+	// line.graphics.endStroke();
+	//
+	// stage.addChild(line);
+
+
+	//draw stuff
+	var fixt = box.getFixtureList();
+	var centerx = box.c_position.c.x;
+	var centery = box.c_position.c.y;
+	boxShape.graphics.beginFill("red").drawRect(0, 0, 1*scale, 1*scale);
+	boxShape.regX = .5*scale;
+	boxShape.regY = .5*scale;
+	boxShape.x = centerx*scale;
+	boxShape.y = centery*scale;
+	boxShape.rotation = box.getAngle()*(180/Math.PI);
+
+	for (var body = world.getBodyList(); body; body = body.getNext()) {
+		for (var fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
+			// draw or update fixture
+		}
+	}
+
+	stage.update();
+}
